7a6169acd317dc60441897c08ec577cf87b66454
prashantkhamitkar <prashantkhamitkar@gmail.com>
2024-02-27 02:11:48 +0530

    making Responsive

---
+++ src/component/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './CSS/Footer.css'
+const Footer = () => {
+  return (
+        
+           <div className='ParentFooter'> <div className='FooterContainer'>
+           <div className='TextDiv'>contact</div>
+           <div className='TextDiv'>facebook</div>
+           <div className='TextDiv'>group</div>
+       </div></div>
+        
+  )
+}
+
+export default Footer
