1a2ab5930bb59103f5aec3cf8e0a7608ca791cf8
prashantkhamitkar <prashantkhamitkar@gmail.com>
2024-02-27 13:42:35 +0530

    all done

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import './CSS/Footer.css'
+
 const Footer = () => {
   return (
         
            <div className='ParentFooter'> <div className='FooterContainer'>
-           <div className='TextDiv'>contact</div>
-           <div className='TextDiv'>facebook</div>
-           <div className='TextDiv'>group</div>
+           <div className='TextDiv'>
+          <img src={'public/asserts/pngwingphone.png'} className='logoclass'/>
+            Toll Free<strong>1800 200 1234</strong></div>
+           <div className='TextDiv'>
+           <img  src="https://img.icons8.com/ios/50/facebook-new.png" alt="facebook-new" className='logoclass'/>
+            <b>www.facebook.com/cripumps</b></div>
+           <div className='TextDiv'>
+           <img className='logoclass' src="https://img.icons8.com/ios/50/globe--v1.png" alt="globe--v1"/>
+            <b>www.crigroups.com</b></div>
        </div></div>
         
   )
